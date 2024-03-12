8d35c0595d0831ca0346ce7436c45b40692e46bf
DonIgnasio1989 <153773838+DonIgnasio1989@users.noreply.github.com>
2024-03-12 10:01:37 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-добавлены задачи на рекурсию
+//добавлены задачи на рекурсию
+static void PrintNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(m + " ");
+        PrintNumbers(m + 1, n);
+    }
+}
